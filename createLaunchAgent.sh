2919206d43f6c2b7fa059d303588858580539ac0
yoopersteeze <31364490+yoopersteeze@users.noreply.github.com>
2018-08-09 12:50:58 -0500

    Update createLaunchAgent.sh
    error checking and code enhancements

--- createLaunchAgent.sh
+++ createLaunchAgent.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
 
-# Get the logged in username
 loggedInUser=$( python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");' )
-
-# Desired name for plist Example: com.mycompany.thing (do not include .plist)
+# loggedInUID=$(id -u $loggedInUser)
+# Make the LaunchAgents directory if does not exist
+/bin/mkdir -p /Users/$loggedInUser/Library/LaunchAgents
+# Desired name for plist Example: com.mycompany.test (do not include .plist)
 plistName="$4"
 # plistName=""
 # Path to script
 scriptPath="$5"
 # scriptPath=""
-# Output the plist
-outputPlist=/Users/$loggedInUser/Library/LaunchAgents/$plistName.plist
-# Make the LaunchAgents directory if need be
-mkdir -p /Users/$loggedInUser/Library/LaunchAgents
+# Error checking
+if [ -z "$plistName" ]; then
+  echo "\$plistName is null, please set a value. Exiting script."
+  exit 1
+else
+  echo "\$plistName is set to: $plistName"
+fi
+if [ -z "$scriptPath" ]; then
+  echo "\$scriptPath is null, please set a value. Exiting script."
+  exit 1
+else
+  echo "\$scriptPath is set to: $scriptPath"
+fi
+outputPlist="/Users/$loggedInUser/Library/LaunchAgents/$plistName.plist"
+# Uncomment below for logging
+# logFile="/var/log/launchd.log"
+# if [ -d "$logFile" ]; then
+#   echo "The file /var/log/launchd.log already exists."
+# else
+#   echo "[WARN] The /var/log/launchd.log did not exit. Creating it now."
+#   touch $logFile
+# fi
 
-cat > $outputPlist <<EOF
+/bin/cat > $outputPlist <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
@@ -28,9 +47,23 @@ cat > $outputPlist <<EOF
   </array>
   <key>RunAtLoad</key>
   <true/>
+  <!--TO ENABLE LOGGING REMOVE THIS LINE AND UNCOMMENT BELOW-->
+  <!--key>StandardOutPath</key>
+  <string>$logFile</string>
+  <key>StandardErrorPath</key>
+  <string>$logFile</string>
+  <key>Debug</key>
+  <true/-->
 </dict>
 </plist>
 EOF
 
-chown root:wheel $outputPlist
-launchctl load -w $outputPlist
+/usr/sbin/chown -R root:wheel $outputPlist
+/bin/chmod 644 $outputPlist
+
+# /usr/bin/su $loggedInUser -c "/bin/launchctl load -w $outputPlist"
+# /bin/launchctl asuser "$loggedInUser" -c /bin/launchctl load -w $outputPlist
+# /bin/launchctl asuser "$loggedInUID" sudo -iu "$loggedInUser" /bin/launchctl load -w $outputPlist
+/bin/launchctl load -w $outputPlist
+# /bin/launchctl bsexec "${loggedInUID}" sudo -iu "${loggedInUser}" "launchctl load -w $outputPlist"
+exit 0
