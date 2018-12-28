e28afa97280a8475098411a47e8caf37c17c93c6
yoopersteeze <31364490+yoopersteeze@users.noreply.github.com>
2018-12-27 22:08:16 -0600

    Update deletePending+FailedCommands.sh

--- deletePending+FailedCommands.sh
+++ deletePending+FailedCommands.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+####################################################################################################
+#
+# THIS SCRIPT IS NOT AN OFFICIAL PRODUCT OF JAMF SOFTWARE
+# AS SUCH IT IS PROVIDED WITHOUT WARRANTY OR SUPPORT
+#
+# BY USING THIS SCRIPT, YOU AGREE THAT JAMF SOFTWARE
+# IS UNDER NO OBLIGATION TO SUPPORT, DEBUG, OR OTHERWISE
+# MAINTAIN THIS SCRIPT
+#
+####################################################################################################
+#
+# DEFINE VARIABLES & READ IN PARAMETERS
+#
+####################################################################################################
+
+read -p "Jamf Pro URL: " server
+read -p "Jamf Pro Username: " username
+read -s -p "Jamf Pro Password: " password
+echo ""
+
+####################################################################################################
+#
+# SCRIPT CONTENTS - DO NOT MODIFY BELOW THIS LINE
+#
+####################################################################################################
+
 server=""
 username=""
 password=""
