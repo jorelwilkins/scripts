06cfe8349f6c0ab30be4797086a44ee25afaac4c
yoopersteeze <31364490+yoopersteeze@users.noreply.github.com>
2019-03-21 10:59:07 -0500

    Add files via upload

---
+++ launchD-chicken-tacos.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+####################################################################################################
+#
+# THIS SCRIPT IS NOT AN OFFICIAL PRODUCT OF JAMF SOFTWARE
+# AS SUCH IT IS PROVIDED WITHOUT WARRANTY OR SUPPORT
+#
+# BY USING THIS SCRIPT, YOU AGREE THAT JAMF SOFTWARE
+# IS UNDER NO OBLIGATION TO SUPPORT, DEBUG, OR OTHERWISE
+# MAINTAIN THIS SCRIPT
+#
+####################################################################################################
+# This script was designed create a LaunchDaemon to say "chicken tacos" every 60 seconds
+
+# Make the LaunchAgents directory if does not exist
+mkdir -p /Library/LaunchDaemons
+
+outputPlist="/Library/LaunchAgents/com.sunnyvale.chickentacos.plist"
+
+/bin/cat > "$outputPlist" <<EOF
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>EnvironmentVariables</key>
+	<dict>
+		<key>PATH</key>
+		<string>/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/sbin</string>
+	</dict>
+	<key>Label</key>
+	<string>com.sunnyvale.chickentacos</string>
+	<key>ProgramArguments</key>
+	<array>
+		<string>/usr/bin/say</string>
+		<string>chicken</string>
+		<string>tacos</string>
+	</array>
+	<key>RunAtLoad</key>
+	<true/>
+	<key>StartInterval</key>
+	<integer>60</integer>
+</dict>
+</plist>
+EOF
+/usr/sbin/chown -R root:wheel "$outputPlist"
+/bin/chmod 644 "$outputPlist"
+/bin/launchctl load -w "$outputPlist"
+
+exit 0
