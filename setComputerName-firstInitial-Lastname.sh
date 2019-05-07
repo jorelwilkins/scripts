531e765668e5da19e5aa68f1802a35a00217e82b
yoopersteeze <31364490+yoopersteeze@users.noreply.github.com>
2019-05-07 09:57:41 -0500

    Add files via upload

---
+++ setComputerName-firstInitial-Lastname.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Source: https://www.jamf.com/jamf-nation/discussions/26912/script-to-rename-computer-name-letter-of-first-name-and-last-name#responseChild159772
+
+#gets current logged in user
+loggedInUser=$( python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");' )
+
+#gets named
+# firstInitial=$(finger -s $loggedInUser | head -2 | tail -n 1 | awk '{print tolower($2)}' | cut -c 1)
+firstInitial=$(finger -s $loggedInUser | head -2 | tail -n 1 | awk '{print tolower($2)}' | cut -c 1)
+lastName=$(finger -s $getUser | head -2 | tail -n 1 | awk '{print tolower($3)}')
+computerName=$firstInitial$lastName"-mac"
+
+#set all the name in all the places
+/usr/sbin/scutil --set ComputerName "$computerName"
+/usr/sbin/scutil --set LocalHostName "$computerName"
+/usr/sbin/scutil --set HostName "$computerName"
+
+# Uncomment below if wanting to use the jamf binary to set all the names
+# /usr/local/bin/jamf setComputerName -name "$computerName"
+
+# Uncomment below if you want to update inventory
+#/usr/local/bin/jamf recon
