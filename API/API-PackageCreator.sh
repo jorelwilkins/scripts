0b44c04e5d92f9ca1d94c18cf58e62b8a930d856
yoopersteeze <ryan.peterson1@icloud.com>
2019-07-18 15:18:00 -0500

    moved to folder

---
+++ API/API-PackageCreator.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+####################################################################################################
+#
+# THIS SCRIPT IS NOT AN OFFICIAL PRODUCT OF JAMF SOFTWARE
+# AS SUCH IT IS PROVIDED WITHOUT WARRANTY OR SUPPORT
+#
+# BY USING THIS SCRIPT, YOU AGREE THAT JAMF SOFTWARE
+# IS UNDER NO OBLIGATION TO SUPPORT, DEBUG, OR OTHERWISE
+# MAINTAIN THIS SCRIPT
+#
+####################################################################################################
+#
+# DESCRIPTION
+# This script will create a package record inside of Jamf Pro. Can be used when a large package
+# such as a 20GB+ Adobe file cannot be uploaded to JCDS. Manually upload the file to a local SMB / HTTPS
+# share, then use this script to create it's record inside of Jamf Pro.
+####################################################################################################
+#
+# DEFINE VARIABLES & READ IN PARAMETERS
+#
+####################################################################################################
+
+read -p "Jamf Pro URL: " server
+read -p "Jamf Pro Username: " username
+read -s -p "Jamf Pro Password: " password
+echo ""
+read "Package Name (including file extension)": packageName				#Name of the package (including file extension)
+####################################################################################################
+#
+# SCRIPT CONTENTS - DO NOT MODIFY BELOW THIS LINE
+#
+####################################################################################################
+# Courtesy of github dot com slash zdorow
+echo "Testing connection to Jamf Pro..."
+test=$(curl --fail -ksu "$username":"$password" "$server"/JSSResource/users -X GET)
+status=$?
+if [ $status -eq 6 ]; then
+	echo ""
+	echo "The Jamf Pro URL is incorrect. Please edit the URL and try again."
+	echo "If the error persists please check permissions and internet connection"
+	echo ""
+	exit 99
+elif [ $status -eq 22 ]; then
+	echo ""
+	echo "Username and/or password is incorrect. Please edit and try again."
+	echo "If the error persists please check permissions and internet connection"
+	echo ""
+	exit 99
+elif [ $status -eq 0 ]; then
+    echo ""
+    echo "Connection test successful! "
+    echo ""
+else
+    echo ""
+    echo "Something went really wrong,"
+    echo "Lets try this again."
+    exit 99
+fi
+####################################################################################################
+#
+# MAIN PROCESS
+#
+####################################################################################################
+
+curl -kvu ${username}:${password} -H "Content-Type: text/xml" ${server}/JSSResource/packages/id/-1 -d "<package><name>${packageName}</name><filename>${packageName}</filename></package>" -X POST
+
+exit 0
