38c72115a4306cce39250142b466f72804149982
yoopersteeze <ryan.peterson1@icloud.com>
2019-08-05 08:34:41 -0500

    update

--- EAs/apnsTopicID-EA.sh
+++ EAs/apnsTopicID-EA.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-realTopic="com.apple.mgmt.External.put.the.real.topic.here"
+realTopic="com.apple.mgmt.External.topic.id.here"
+# machineTopic=$(/System/Library/PrivateFrameworks/ApplePushService.framework/apsctl status | grep "com.apple.mgmt.External." | sed -n '1!p' | awk '{print $5}')
+machineTopic=$(/System/Library/PrivateFrameworks/ApplePushService.framework/apsctl status | grep "com.apple.mgmt.External." | head -n 1 | awk '{print $2}')
+# echo "Real topic: #realTopic "
 
-machineTopic=$(/System/Library/PrivateFrameworks/ApplePushService.framework/apsctl status | grep "com.apple.mgmt.External." | sed -n '1!p' | awk '{print $5}')
-
-if [ $machineTopic == $realTopic ]; then
-	echo "<result>Match</result>"
+# echo "Machine topic: $machineTopic "
+if [ "$machineTopic" == $realTopic ]; then
+  echo "<result>Match</result>"
 else
-	echo "<result>Does not match</result>"
+  echo "<result>Does not match</result>"
 fi
