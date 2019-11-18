8847a6515b807c28ff7fb7cf77d32aa6109ebe8a
yoopersteeze <31364490+yoopersteeze@users.noreply.github.com>
2019-11-18 12:35:34 -0600

    Update API-PackageCreator.sh

--- API/API-PackageCreator.sh
+++ API/API-PackageCreator.sh
@@ -30,37 +30,6 @@ read "Package Name (including file extension)": packageName				#Name of the pack
 # SCRIPT CONTENTS - DO NOT MODIFY BELOW THIS LINE
 #
 ####################################################################################################
-# Courtesy of github dot com slash zdorow
-echo "Testing connection to Jamf Pro..."
-test=$(curl --fail -ksu "$username":"$password" "$server"/JSSResource/users -X GET)
-status=$?
-if [ $status -eq 6 ]; then
-	echo ""
-	echo "The Jamf Pro URL is incorrect. Please edit the URL and try again."
-	echo "If the error persists please check permissions and internet connection"
-	echo ""
-	exit 99
-elif [ $status -eq 22 ]; then
-	echo ""
-	echo "Username and/or password is incorrect. Please edit and try again."
-	echo "If the error persists please check permissions and internet connection"
-	echo ""
-	exit 99
-elif [ $status -eq 0 ]; then
-    echo ""
-    echo "Connection test successful! "
-    echo ""
-else
-    echo ""
-    echo "Something went really wrong,"
-    echo "Lets try this again."
-    exit 99
-fi
-####################################################################################################
-#
-# MAIN PROCESS
-#
-####################################################################################################
 
 curl -kvu ${username}:${password} -H "Content-Type: text/xml" ${server}/JSSResource/packages/id/-1 -d "<package><name>${packageName}</name><filename>${packageName}</filename></package>" -X POST
 
